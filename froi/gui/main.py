e04dbdc4d40dfaa42d9cbf7e8d9fd3305cda9414
Lijie Huang <huanglijie.seal@gmail.com>
2015-07-12 16:27:05 +0800

    update

--- froi/gui/main.py
+++ froi/gui/main.py
@@ -231,8 +231,9 @@ class BpMainWindow(QMainWindow):
         self._actions['new_image'].setEnabled(False)
 
         # Duplicate image
-        self._actions['duplicate_image'].QAction(self.tr("Duplicate"), self)
-        self._actions['duplicate_image'].triggered.connect(self._duplicate_image)
+        self._actions['duplicate_image'] = QAction(self.tr("Duplicate"), self)
+        self._actions['duplicate_image'].triggered.connect(
+                                        self._duplicate_image)
         self._actions['duplicate_image'].setEnabled(False)
 
         # Save image
@@ -653,6 +654,7 @@ class BpMainWindow(QMainWindow):
                 #self.setUnifiedTitleAndToolBarOnMac(True)
                 # change button status
                 self._actions['save_image'].setEnabled(True)
+                self._actions['duplicate_image'].setEnabled(True)
                 #self._actions['ld_lbl'].setEnabled(True)
                 #self._actions['ld_glbl'].setEnabled(True)
                 self._actions['new_image'].setEnabled(True)
@@ -767,6 +769,7 @@ class BpMainWindow(QMainWindow):
         self._actions['remove_image'].setEnabled(False)
         self._actions['new_image'].setEnabled(False)
         self._actions['save_image'].setEnabled(False)
+        self._actions['duplicate_image'].setEnabled(False)
         #self._actions['ld_glbl'].setEnabled(False)
         #self._actions['ld_lbl'].setEnabled(False)
         self._actions['close'].setEnabled(False)
@@ -812,7 +815,6 @@ class BpMainWindow(QMainWindow):
         self.file_menu.addSeparator()
         self.file_menu.addAction(self._actions['new_image'])
         self.file_menu.addAction(self._actions['remove_image'])
-        # TODO: add duplicate image function
         self.file_menu.addAction(self._actions['duplicate_image'])
         self.file_menu.addAction(self._actions['save_image'])
         #self.file_menu.addAction(self._actions['ld_lbl'])
