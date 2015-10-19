454331872fac79922d8f48243fd34bb6bb4a0263
zhouguangfu <zhougf0908@gmail.com>
2015-10-19 22:56:17 +0800

    Add fsl register method.

--- froi/gui/component/registervolume.py
+++ froi/gui/component/registervolume.py
@@ -13,6 +13,8 @@ class RegisterVolumeDialog(QDialog):
         super(RegisterVolumeDialog, self).__init__(parent)
 
         self._model = model
+        self._temp_dir = None
+
         self._init_gui()
         self._create_actions()
 
@@ -77,22 +79,54 @@ class RegisterVolumeDialog(QDialog):
         self.input_button.clicked.connect(self._input_browse)
         self.reference_button.clicked.connect(self._reference_browse)
         self.output_button.clicked.connect(self._output_browse)
+        self.register_button.clicked.connect(self._register)
 
     def _input_browse(self):
-        input_dir = str(QFileDialog.getExistingDirectory(self, "Select Input Volume Directory"))
-        self.input_dir.setText(input_dir)
+        if self._temp_dir == None:
+            temp_dir = QDir.currentPath()
+        else:
+            temp_dir = self._temp_dir
+        file_name = QFileDialog.getOpenFileName(self,
+                                                'Add new file',
+                                                temp_dir,
+                                                "Nifti files (*.nii *.nii.gz)")
+        import sys
+        if not file_name.isEmpty():
+            if sys.platform == 'win32':
+                file_path = unicode(file_name).encode('gb2312')
+            else:
+                file_path = str(file_name)
+        self.input_dir.setText(file_path)
+        self._temp_dir = file_path
 
     def _reference_browse(self):
-        reference_dir = str(QFileDialog.getExistingDirectory(self, "Select Reference Volume Directory"))
-        self.reference_dir.setText(reference_dir)
+        if self._temp_dir == None:
+            temp_dir = QDir.currentPath()
+        else:
+            temp_dir = self._temp_dir
+        file_name = QFileDialog.getOpenFileName(self,
+                                                'Add new file',
+                                                temp_dir,
+                                                "Nifti files (*.nii *.nii.gz)")
+        import sys
+        if not file_name.isEmpty():
+            if sys.platform == 'win32':
+                file_path = unicode(file_name).encode('gb2312')
+            else:
+                file_path = str(file_name)
+        self.reference_dir.setText(file_path)
+        self._temp_dir = file_path
 
     def _output_browse(self):
-        file_path = temp_dir = str(QDir.currentPath())
+        if self._temp_dir == None:
+            temp_dir = QDir.currentPath()
+        else:
+            temp_dir = self._temp_dir
         file_types = "Compressed NIFTI file(*.nii.gz);;NIFTI file(*.nii)"
         path,filter = QFileDialog.getSaveFileNameAndFilter(
             self,
             'Save image as...',
-            file_path,
+            temp_dir,
             file_types,)
         if not path.isEmpty():
             self.output_dir.setText(path)
@@ -104,12 +138,125 @@ class RegisterVolumeDialog(QDialog):
                 self._temp_dir = os.path.dirname(unicode(path, 'gb2312'))
             else:
                 path = str(path)
+        self._temp_dir = path
         print 'Save output file path: ', path
 
     def _register(self):
-        if self.input_dir is '' or self.reference_dir is '':
-            QMessageBox.warning('Input volume or reference volume directory is empty!')
-            return
+        if str(self.input_dir.text()) is '' or \
+            str(self.reference_dir.text()) is '' or \
+            str(self.output_dir.text()) is '':
+            QMessageBox.warning('Complete the filepath information!')
+
+        rm = RegisterMethod()
+        rm.fsl_register(str(self.input_dir.text()),
+                        str(self.reference_dir.text()),
+                        str(self.output_dir.text()))
+
+        print 'self.output_dir: ', str(self.output_dir.text())
+        self.done()
+
+
+class RegisterMethod(object):
+    def __init__(self, parent=None):
+        pass
+
+    def afni_register(self, input_volume_filename, output_volume_filename):
+        '''AFNI Registration'''
+
+        from nipype.interfaces import afni as afni
+        # volreg = afni.Volreg()
+        # volreg.inputs.in_file = input_volume_filename
+        # volreg.inputs.args = '-Fourier -twopass'
+        # volreg.inputs.zpad = 4
+        # volreg.inputs.outputtype = "NIFTI"
+        # # volreg.cmdline  '3dvolreg -Fourier -twopass -1Dfile functional.1D -1Dmatrix_save functional.aff12.1D -prefix functional_volreg.nii -zpad 4 -maxdisp1D functional_md.1D functional.nii'
+        # res = volreg.run()
+
+        from nipype.interfaces import afni as afni
+        allineate = afni.Allineate()
+        allineate.inputs.in_file = input_volume_filename
+        allineate.inputs.out_file= output_volume_filename
+        allineate.inputs.in_matrix= 'cmatrix.mat'
+        res = allineate.run()
+
+        print 'res: ', res
+
+        return res
+
+
+    def fsl_register(self, input_volume_filename, reference_volume_filename, output_volume_filename):
+        '''FSL Registration'''
+        # Use FSL FLIRT for coregistration.
+
+        from nipype.interfaces import fsl
+        from nipype.testing import example_data
+        flt = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+        flt.inputs.in_file = input_volume_filename
+        flt.inputs.reference = reference_volume_filename
+        flt.inputs.output_type = "NIFTI_GZ"
+        flt.out_matrix_file = './result/fsl_matrix.mat'
+        flt.out_file = output_volume_filename
+        print flt.cmdline
+        # flt.cmdline
+        # 'flirt -in structural.nii -ref mni.nii -out structural_flirt.nii.gz -omat structural_flirt.mat -bins 640 -searchcost mutualinfo'
+
+        res = flt.run()
+        print res
+        print 'Register end!'
+
+        return res
+
+
+    def freesurfer_register(self, input_volume, output_volume_filename):
+        '''FreeSurfer Registration'''
+        # http://www.mit.edu/~satra/nipype-nightly/interfaces/generated/nipype.interfaces.freesurfer.preprocess.html
+
+        from nipype.interfaces.freesurfer import RobustRegister
+        reg = RobustRegister()
+        reg.inputs.source_file = input_volume
+        reg.inputs.target_file = output_volume_filename
+        reg.inputs.auto_sens = True
+        reg.inputs.init_orient = True
+        # reg.cmdline
+        # 'mri_robust_register --satit --initorient --lta structural_robustreg.lta --mov structural.nii --dst T1.nii'
+        reg.run()
+
+        return reg
+
+    def spm_register(self, input_volume_filename, output_volume_filename):
+        '''SPM Registration'''
+        # http://www.mit.edu/~satra/nipype-nightly/interfaces/generated/nipype.interfaces.spm.preprocess.html
+
+        import nipype.interfaces.matlab as mlab      # how to run matlab
+
+        """
+
+        Preliminaries
+        -------------
+
+        Set any package specific configuration. The output file format
+        for FSL routines is being set to uncompressed NIFTI and a specific
+        version of matlab is being used. The uncompressed format is required
+        because SPM does not handle compressed NIFTI.
+        """
+
+        # Set the way matlab should be called
+        # mlab.MatlabCommand.set_default_matlab_cmd("matlab -nodesktop -nosplash")
+        # If SPM is not in your MATLAB path you should add it here
+        # mlab.MatlabCommand.set_default_paths('/nfs/j3/userhome/zhouguangfu/workingdir/spm')
+
+        res = mlab.MatlabCommand(script='which(spm)',
+                        paths=['/nfs/j3/userhome/zhouguangfu/workingdir/spm'],
+                        mfile=False).run()
+        print res.runtime.stdout
+
+        import nipype.interfaces.spm as spm
+        coreg = spm.Coregister()
+        coreg.inputs.target = output_volume_filename
+        coreg.inputs.source = input_volume_filename
+
+        coreg.run()
+
 
 
 
