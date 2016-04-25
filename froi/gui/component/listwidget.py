0605c2a0fe2457e3703a4a7548299fc2c1e9aca0
zhouguangfu <zhougf0908@gmail.com>
2016-04-25 14:09:04 +0800

    Set the method setKeyboardTracking False in QSpinBox.

--- froi/gui/component/listwidget.py
+++ froi/gui/component/listwidget.py
@@ -218,6 +218,7 @@ class LayerView(QWidget):
         self._up_button.clicked.connect(self._up_action)
         self._down_button.clicked.connect(self._down_action)
         self._volume_index_spinbox.valueChanged.connect(self._set_time_point)
+        self._volume_index_spinbox.setKeyboardTracking(False)
         # set voxel ijk position
         self._coord_x.valueChanged.connect(self.set_cross_pos)
         self._coord_y.valueChanged.connect(self.set_cross_pos)
